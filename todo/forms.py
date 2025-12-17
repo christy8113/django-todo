0ce9350d079342aefebf26fd60cd8080809756aa
Christy Philip <christy.philip2004@gmail.com>
2025-12-17 20:48:37 +0530

    Improve UI with Bootstrap colors and styling

--- todo/forms.py
+++ todo/forms.py
@@ -5,3 +5,14 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['subject', 'notes']
+        widgets = {
+            'subject': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter subject'
+            }),
+            'notes': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter notes',
+                'rows': 4
+            }),
+        }
