b04da1a23f523811baf20c104dbeb600102ed372
Christy Philip <christy.philip2004@gmail.com>
2025-12-19 10:43:24 +0530

    Improve UI with Bootstrap colors and styling

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('add/', views.todo_create, name='todo_create'),
+    path('view/<int:id>/', views.todo_detail, name='todo_detail'),  # 👈 NEW
     path('edit/<int:id>/', views.todo_update, name='todo_update'),
     path('delete/<int:id>/', views.todo_delete, name='todo_delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,15 @@
-# Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import TodoForm
 
-# List To-Dos
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todo/list.html', {'todos': todos})
 
-# Add To-Do
+def todo_detail(request, id):   # 👈 NEW
+    todo = get_object_or_404(Todo, id=id)
+    return render(request, 'todo/detail.html', {'todo': todo})
+
 def todo_create(request):
     form = TodoForm(request.POST or None)
     if form.is_valid():
@@ -16,7 +17,6 @@ def todo_create(request):
         return redirect('todo_list')
     return render(request, 'todo/form.html', {'form': form})
 
-# Edit To-Do
 def todo_update(request, id):
     todo = get_object_or_404(Todo, id=id)
     form = TodoForm(request.POST or None, instance=todo)
@@ -25,7 +25,6 @@ def todo_update(request, id):
         return redirect('todo_list')
     return render(request, 'todo/form.html', {'form': form})
 
-# Delete To-Do
 def todo_delete(request, id):
     todo = get_object_or_404(Todo, id=id)
     todo.delete()
